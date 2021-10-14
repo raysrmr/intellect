c841822b4e5572b1e7408686e6070b1c72a63228
raysrmr <rays.rmr@gmail.com>
2021-10-14 14:33:26 +0530

    first commit

---
+++ js/one.js
@@ -0,0 +1,15 @@
+function add(){
+    var a=10;
+    var b=20;
+    var c=a+b;
+    document.write(c)
+}
+
+function add1(){
+    var a=10;
+    var b=20;
+    var c=a+b;
+    return c;
+}
+add();
+document.write(add1())
